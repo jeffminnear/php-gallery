c38133a1af16d7474d029a609996f69c2b37be6a
Jeff Minnear <jeffminnear@gmail.com>
2016-09-29 13:55:44 -0400

    Open connection on Database __construct()

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -6,6 +6,10 @@ class Database {
 
   public $connection;
 
+  function __construct() {
+    $this->open_db_connection();
+  }
+
   public function open_db_connection() {
     $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
@@ -17,6 +21,5 @@ class Database {
 }
 
 $database = new Database();
-$database->open_db_connection();
 
 ?>
