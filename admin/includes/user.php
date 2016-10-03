36d32fd676f50ae9df67f2d5d38719beabc427d4
Jeff Minnear <jeffminnear@gmail.com>
2016-10-03 17:18:03 -0400

    Extract a find_this_query method, and create public properties in User class

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -1,21 +1,30 @@
 <?php
 
   class User {
+
+    public $id;
+    public $username;
+    public $password;
+    public $first_name;
+    public $last_name;
+
     // returns all users as a mysqli_result object
     public static function find_all_users() {
-      global $database;
-
-      $result_set = $database->query("SELECT * FROM users");
-      return $result_set;
+      return self::find_this_query("SELECT * FROM users");
     }
 
     // finds a single user by id and returns it as an array
     public static function find_user_by_id($id) {
-      global $database;
-
-      $result = $database->query("SELECT * FROM users WHERE id=$id LIMIT 1");
+      $result = self::find_this_query("SELECT * FROM users WHERE id=$id LIMIT 1");
       $user = mysqli_fetch_array($result);
       return $user;
+    }
+
+    public static function find_this_query($sql) {
+      global $database;
+
+      $result_set = $database->query($sql);
+      return $result_set;
     }
 
   }
