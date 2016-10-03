361f39c1131381988fdca9b83ffbec3cff0bba2e
Jeff Minnear <jeffminnear@gmail.com>
2016-10-03 12:06:06 -0400

    Create User class

---
+++ admin/includes/user.php
@@ -0,0 +1,13 @@
+<?php
+
+  class User {
+    public static function find_all_users() {
+      global $database;
+
+      $result_set = $database->query('SELECT * FROM users');
+      return $result_set;
+    }
+
+  }
+
+?>
